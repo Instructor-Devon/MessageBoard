79d375c04efc6f2f61d85aea2df1814f1f1e1ef9
Devon <=>
2019-03-07 14:53:07 -0800

    remove dbconnector ref

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FakeReddit.Models;
-using DbConnection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
 
